b2839018e043ceb1de872a278279a198facaf0cb
Mad Wiki <wudifengxiang@gamil.com>
2018-03-26 06:20:49 +0800

    support eth and all erc20 tokens

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # crypto-income
 Get incomes from given wallet
+
+## Usage
+
+### Erc20 example:
+
+```javascript
+getIncome({
+  walletId: '0x7dfdb02a1a......f95c3cc6abb97', // Target wallet
+  startBlock: '5283836',
+  coinType: 'erc20',
+  contractAddr: '0x8B40761142B9aa6dc8964e61D0585995425C3D94', // Tripio's address
+  callback: (error, data) => {
+    if (!error && !data.empty) {
+      console.log('Target wallet got income:', data.totalValue / Math.pow(10, data.decimals) + 'trio'); 
+    }
+  },
+});
+
+
+> Target wallet got income: 26833.088 trio
+```
+
+### Eth example:
+
+```javascript
+getIncome({
+  walletId: '0x7dfdb02a1a......f95c3cc6abb97', // Target wallet
+  startBlock: '5283836',
+  coinType: 'eth',
+  callback: (error, data) => {
+    if (!error && !data.empty) {
+      console.log('target wallet got income:', data.totalValue / Math.pow(10, data.decimals) + 'eth'); 
+    }
+  },
+});
+
+
+> Target wallet got income: 0.31 eth
+```
+
+
+
+#### Return Value
+
+```javascript
+// error object
+{ error: 'text' };
+
+// data object
+{
+  totalValue,
+  currentBlock,
+  minConfirmations,
+  decimals,
+  txs: [
+    {
+      hash, // transaction hash
+      from, // sender
+      to, // receiver
+      value, // the raw value of the coin, you need use decimals to calculate the human readable value for customers.
+      blockNumber,
+      timeStamp,
+      confirmations,
+  	},
+    ...
+  ],
+}
+```
+
---
+++ index.js
@@ -0,0 +1,156 @@
+import { erc20Abi, ethDecimals } from './constant';
+import InputDataDecoder from 'ethereum-input-data-decoder';
+import Web3 from 'web3';
+import async from 'async';
+import request from 'request';
+
+const web3 = new Web3(
+  new Web3.providers.HttpProvider('https://api.myetherapi.com/eth')
+);
+
+const erc20DecimalsMap = {};
+
+export function getIncome({
+  walletId,
+  startBlock,
+  coinType,
+  callback,
+  contractAddr,
+}) {
+  let contract;
+
+  if (!walletId || !coinType || !startBlock || !callback) {
+    return callback({ error: 'Lack of params' });
+  }
+
+  if (coinType === 'erc20') {
+    if (!contractAddr) {
+      return callback({ error: 'contractAddr is required' });
+    }
+    contract = new web3.eth.Contract(erc20Abi, contractAddr);
+  }
+
+  async.waterfall([
+    (stepCallback) => {
+      if (coinType === 'erc20' && !erc20DecimalsMap[contractAddr]) {
+        contract
+        .methods
+        .decimals()
+        .call((err, result) => {
+          if (err) {
+            return stepCallback(err);
+          }
+          erc20DecimalsMap[contractAddr] = Number(result);
+          stepCallback();
+        });
+      } else {
+        stepCallback();
+      }
+    },
+    (stepCallback) => {
+      request({
+        url: 'https://api.etherscan.io/api?module=account&action=txlist' +
+        `&address=${coinType === 'erc20' ? contractAddr : walletId}` +
+        `&startblock=${startBlock}&endblock=999999999` +
+        '&sort=desc&apikey=AXQE6T8J5F4ZD2QDYWTUJFDSSK5UQUUGSN'
+      }, (err, res, body) => {
+        if (err) {
+          stepCallback(err);
+        }
+        const bodyObj = JSON.parse(body);
+        const rawTxs = bodyObj.result;
+
+        if (rawTxs && rawTxs.length > 0) {
+
+          if (coinType === 'eth') {
+            const txs = rawTxs.filter(tx =>
+              tx.to.toLowerCase() === walletId.toLowerCase()
+            );
+
+            if (txs.length > 0) {
+              const totalValue = txs.reduce((value, tx) =>
+              value + Number(tx.value), 0);
+              const currentBlock = txs[0].blockNumber;
+              const minConfirmations = Number(txs[0].confirmations);
+
+              callback(null, {
+                totalValue,
+                currentBlock,
+                minConfirmations,
+                decimals: ethDecimals,
+                txs: txs.map(tx => ({
+                  hash: tx.hash,
+                  from: tx.from,
+                  to: tx.to,
+                  blockNumber: tx.blockNumber,
+                  timeStamp: tx.timeStamp,
+                  confirmations: tx.confirmations,
+                })),
+              });
+            } else {
+              callback(null, { empty: true });
+            }
+          } else if (coinType === 'erc20') {
+            const decoder = new InputDataDecoder(erc20Abi);
+
+            const txs = rawTxs.filter(tx =>
+              tx.to.toLowerCase() === contractAddr.toLowerCase()
+            ).map(tx => {
+              const inputData = decoder.decodeData(tx.input);
+
+              return inputData.name === 'transfer' ? {
+                hash: tx.hash,
+                from: tx.from,
+                to: `0x${inputData.inputs[0]}`,
+                value: inputData.inputs[1].toString(10),
+                blockNumber: tx.blockNumber,
+                timeStamp: tx.timeStamp,
+                confirmations: tx.confirmations,
+              } : null;
+            })
+            .filter(tx => tx && tx.to.toLowerCase() === walletId.toLowerCase());
+
+            if (txs.length > 0) {
+              const totalValue = txs.reduce((value, tx) =>
+              value + Number(tx.value), 0);
+              const currentBlock = txs[0].blockNumber;
+              const minConfirmations = Number(txs[0].confirmations);
+
+              callback(null, {
+                totalValue,
+                currentBlock,
+                minConfirmations,
+                decimals: erc20DecimalsMap[contractAddr],
+                txs: txs.map(tx => ({
+                  hash: tx.hash,
+                  from: tx.from,
+                  to: tx.to,
+                  value: tx.value,
+                  blockNumber: tx.blockNumber,
+                  timeStamp: tx.timeStamp,
+                  confirmations: tx.confirmations,
+                })),
+              });
+            } else {
+              callback(null, { empty: true });
+            }
+          } else {
+            callback({ error: 'coinType is not supported' });
+          }
+
+        } else {
+          callback(null, { empty: true });
+        }
+      });
+    }
+  ], error => {
+    console.log('error', error);
+    return getIncome({
+      walletId,
+      startBlock,
+      coinType,
+      callback,
+      contractAddr,
+    });
+  });
+}
